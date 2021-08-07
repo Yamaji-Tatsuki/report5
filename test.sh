dcd48cc9b39dd2f394b0726519675ad54d69fd1a
yamaji <a2141ty@aiit.ac.jp>
2021-08-07 09:24:59 +0900

    commit the test script"test.sh" However, it is still been editted for some parts.

--- test.sh
+++ test.sh
@@ -1,1 +1,25 @@
-test
+#!/bin/bash
+#stil editting
+read -p > num1 num2
+if [$num1...] ; then # if num1 is not string
+
+	output1=$(./gcd.sh,  ) #引数を./gcdに渡す
+	if [$output1 = "please input integer !!"] ; then
+		echo "ok"
+		exit 0
+	else
+		echo "NG"
+		exit 1
+
+	if [$num2...] #if num2 is not string
+	output2=$(./gcd.sh,  ) #引数を./gcdに渡す
+	if [$output2 = "please input integer !!"] ; then                                                           echo "ok"                                                                                          exit 0
+	else
+		echo "NG"  
+		exit 1
+
+else
+	echo "ok"
+	exit 0
+fi
+
